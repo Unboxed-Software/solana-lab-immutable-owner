94a0505eabce58013d1b5ea9816226e5b0ae44a8
PBillingsby <philbillingsby@gmail.com>
2024-03-06 18:32:24 -0500

    add createAssociatedTokenAccount tests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { AuthorityType, TOKEN_2022_PROGRAM_ID, createMint, setAuthority } from "@solana/spl-token";
+import { AuthorityType, TOKEN_2022_PROGRAM_ID, createAssociatedTokenAccount, createMint, setAuthority } from "@solana/spl-token";
 import { Connection, Keypair, PublicKey, clusterApiUrl } from "@solana/web3.js";
 import { initializeKeypair } from "./keypair-helper";
 import { createTokenAccountWithImmutableOwner } from "./token-helper";
@@ -34,6 +34,31 @@ async function testTryingToTransferOwner(inputs: TransferOwnerInputs) {
   }
 }
 
+async function testTryingToTransferOwnerWithAssociatedTokenAccount(inputs: TransferOwnerInputs) {
+  const { connection, payer, tokenAccount, newAuthority } = inputs;
+  try {
+    // Attempt to change owner
+    await setAuthority(
+      connection,
+      payer,
+      tokenAccount,
+      payer.publicKey,
+      AuthorityType.AccountOwner,
+      newAuthority,
+      undefined,
+      undefined,
+      TOKEN_2022_PROGRAM_ID
+    );
+
+    console.error("You should not be able to change the owner of the account.");
+
+  } catch (error) {
+    console.log(
+      `✅ - We expected this to fail because the associated token account is immutable, and cannot change owner.`
+    );
+  }
+}
+
 (async () => {
   const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
   const payer = await initializeKeypair(connection);
@@ -56,12 +81,24 @@ async function testTryingToTransferOwner(inputs: TransferOwnerInputs) {
     TOKEN_2022_PROGRAM_ID,
   );
 
+
+  // Explicitly creating immutable owner token account with instructions
   const createOurTokenAccountSignature = await createTokenAccountWithImmutableOwner(
     connection,
     mint,
     payer,
     payer,
     ourTokenAccountKeypair
+  );
+
+  // Creating token account with default immutable owner
+  const associatedTokenAccount = await createAssociatedTokenAccount(
+    connection,
+    payer,
+    mint,
+    payer.publicKey,
+    undefined,
+    TOKEN_2022_PROGRAM_ID,
   );
 
   {
@@ -72,5 +109,14 @@ async function testTryingToTransferOwner(inputs: TransferOwnerInputs) {
       tokenAccount: ourTokenAccount,
       newAuthority: otherOwner.publicKey,
     });
+  }
+
+  {
+    await testTryingToTransferOwnerWithAssociatedTokenAccount({
+      connection,
+      payer,
+      tokenAccount: associatedTokenAccount,
+      newAuthority: otherOwner.publicKey
+    })
   }
 })()
